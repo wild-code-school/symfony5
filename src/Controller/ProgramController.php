93b676e52e7a147555e6129fbc114913feeb5705
Dldvlpr <dormoy.dev@gmail.com>
2022-05-24 15:53:47 +0200

    add show function, view and path

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use phpDocumentor\Reflection\DocBlock\Tags\Return_;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
 
-        return $this->render('program/index.html.twig.html.twig', ['website' => 'wild Series',]);
+        return $this->render('Wild/index.html.twig', ['website' => 'wild Series',]);
     }
+
+    #[Route('/{id}', methods: ['GET'], requirements: ['id' => '\d'] ,name: 'show')]
+    public function show(int $id): Response
+    {
+
+        return $this->render('Wild/show.html.twig', ['id' => $id,]);
 }
+}
+
