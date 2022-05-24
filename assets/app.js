b1d80571eed8c79b5b7fad3ee9422a543d0cb13a
Dldvlpr <dormoy.dev@gmail.com>
2022-05-24 11:32:27 +0200

    add bootstrap and navbar

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,11 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
+console.log('Hello Webpack Encore !')
+import studio from './images/studio.png';
 
+let html = `<img src="${studio}" alt="ACME logo">`;
 // start the Stimulus application
 import './bootstrap';
