5bd2fa9c0bd0c4c38ab9a9709bb15015ff2d04bc
dldvlpr <dormoy.dev@gmail.com>
2022-05-26 16:07:09 +0200

    correction use

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
